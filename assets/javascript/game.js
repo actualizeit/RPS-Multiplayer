5f17ac9fb54ae05142e11e8d5bc141ce56791f9b
dbell <greenadvisor@gmail.com>
2019-08-18 21:42:40 -0400

    final touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,198 +1,232 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBTC_DEy2hw-WLtbxgQOnLEVIVWKgV1ty4",
-    authDomain: "rps-game-46a9a.firebaseapp.com",
-    databaseURL: "https://rps-game-46a9a.firebaseio.com",
-    projectId: "rps-game-46a9a",
-    storageBucket: "",
-    messagingSenderId: "412874592967",
-    appId: "1:412874592967:web:5bec7f6ddb8e44f8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+var playerNumber = 0
+var choices = ["Rock", "Paper", "Scissors"];
+var playerChoice = ""
+var oppChoice = ""
+var wins = 0
+var losses = 0 
+var LocalTies = 0
+var playerOneChoice = ""
+var playerTwoChoice = ""
+var blank = ""
 
-  // Create a variable to reference the database
-  var database = firebase.database();
+var playerOneWins = 0
+var playerTwoWins = 0
+var ties = 0
+var DBOutput = []
 
-  // connectionsRef references a specific location in our database.
-  // All of our connections will be stored in this directory.
-  var connectionsRef = database.ref("/connections");
+var firebaseConfig = {
+  apiKey: "AIzaSyBTC_DEy2hw-WLtbxgQOnLEVIVWKgV1ty4",
+  authDomain: "rps-game-46a9a.firebaseapp.com",
+  databaseURL: "https://rps-game-46a9a.firebaseio.com",
+  projectId: "rps-game-46a9a",
+  storageBucket: "",
+  messagingSenderId: "412874592967",
+  appId: "1:412874592967:web:5bec7f6ddb8e44f8"
+};
 
-  // '.info/connected' is a special location provided by Firebase that is updated
-  // every time the client's connection state changes.
-  // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-  var connectedRef = database.ref(".info/connected");
-// When the client's connection state changes...
-  connectedRef.on("value", function(snap) {
+firebase.initializeApp(firebaseConfig);
 
-    // If they are connected..
-    if (snap.val()) {
-  
-      // Add user to the connections list.
-      var con = connectionsRef.push(true);
-      // Add a variable to hold the game info
-      var gg = database.ref("/gameInfo")
+var database = firebase.database();
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
 
-      // Remove user from the connection list and elete game info when someone disconnects.
-      con.onDisconnect().remove();
-      gg.onDisconnect().remove();
-    }
-  });
+connectedRef.on("value", function(snap) {
+if (snap.val()) {
+  var con = connectionsRef.push(true);
+  var gg = database.ref("/gameInfo")
+  con.onDisconnect().remove();
+  gg.onDisconnect().remove();
+}
+});
 
-  var playerNumber = 0
 
-  connectionsRef.on("value", function(snap) {
-    if(playerNumber == 0){
+
+connectionsRef.on("value", function(snap) {
+  if(playerNumber == 0){
     $("#playerNumber").text(snap.numChildren());
     playerNumber = snap.numChildren()
-    console.log(playerNumber)
-    }
-    if(snap.numChildren() < 2){    
+  }
+  if(snap.numChildren() < 2){    
     $("#oppChoice").text("Waiting for an Opponent")
-    }else{
-    $("#oppChoice").text("Someone is rdy to rock lol, hit 'r', 'p', or 's'")
-    }
-  });
+  }else{
+    $("#oppChoice").text("Someone is rdy to rock lol, click a button to make your choice!")
+  }
+});
 
-  var choices = ["r", "p", "s"];
-  var playerChoice = ""
-  var oppChoice = ""
-  var wins = 0
-  var losses = 0 
-  var LocalTies = 0
-  var playerOneChoice = ""
-  var playerTwoChoice = ""
-  var blank = ""
+connectionsRef.on("value", function(snap) {
 
-  var playerOneWins = 0
-  var playerTwoWins = 0
-  var ties = 0
-
-  // When first loaded or when the connections list changes...
-  connectionsRef.on("value", function(snap) {
-
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
-    $("#connected").text(snap.numChildren());
+  $("#connected").text(snap.numChildren());
     if(snap.numChildren() < 3){
-        database.ref("/gameInfo").set({
-            playerOneChoice: playerOneChoice,
-            playerTwoChoice: playerTwoChoice,
-            playerOneWins: playerOneWins,
-            playerTwoWins: playerTwoWins,
-            ties: ties,
+      database.ref("/gameInfo").set({
+        playerOneChoice: playerOneChoice,
+        playerTwoChoice: playerTwoChoice,
+        playerOneWins: playerOneWins,
+        playerTwoWins: playerTwoWins,
+        ties: ties,
+      });
+    }
+});
+
+database.ref("/gameInfo").on("value", function(snapshot) {
+  var p1c = snapshot.child("playerOneChoice").val()
+  var p2c = snapshot.child("playerTwoChoice").val()
+  DBOutput = snapshot
+
+  if (choices.includes(p1c) && choices.includes(p2c)){        
+    if ((p1c === "Rock" && p2c === "Scissors") ||
+    (p1c === "Scissors" && p2c === "Paper") || 
+    (p1c === "Paper" && p2c === "Rock")) {
+      playerOneWins++;
+      $("#choice").text("Player One Chose " + snapshot.val().playerOneChoice + " and Player Two Chose " + snapshot.val().playerTwoChoice + ". Player One Wins!")
+      $("#oppChoice").text("Make a another choice to play again!");
+      database.ref("/gameInfo").update({
+        playerOneWins: playerOneWins,
+        playerOneChoice: blank,
+        playerTwoChoice: blank,
+      });
+    } else if (snapshot.val().playerOneChoice === snapshot.val().playerTwoChoice) {
+        ties++;
+        $("#choice").text("Player One Chose " + snapshot.val().playerOneChoice + " and Player Two Chose " + snapshot.val().playerTwoChoice + ". It's a tie!")
+        $("#oppChoice").text("Make a another choice to play again!");
+        database.ref("/gameInfo").update({
+          ties: ties,
+          playerOneChoice: blank,
+          playerTwoChoice: blank,
         });
-    }
-    console.log(playerNumber)
-  });
-
-
-  database.ref("/gameInfo").on("value", function(snapshot) {
-    var p1c = snapshot.child("playerOneChoice").val()
-    var p2c = snapshot.child("playerTwoChoice").val()
-
-
-    // If Firebase has a highPrice and highBidder stored (first case)
-    if (choices.includes(p1c) && choices.includes(p2c)){
-
-      // Set the local variables for the player choices equal to the stored values in firebase.
-        
-        if ((p1c === "r" && p2c === "s") ||
-        (p1c === "s" && p2c === "p") || 
-        (p1c === "p" && p2c === "r")) {
-            playerOneWins++;
-            database.ref("/gameInfo").update({
-                playerOneWins: playerOneWins,
-                playerOneChoice: blank,
-                playerTwoChoice: blank,
-                    });
-        } else if (snapshot.val().playerOneChoice === snapshot.val().playerTwoChoice) {
-            ties++;
-            database.ref("/gameInfo").update({
-                ties: ties,
-                playerOneChoice: blank,
-                playerTwoChoice: blank,
-            });
-            localTies = snapshot.val().ties;
-            $("#wins").text(wins);
-            $("#losses").text(losses);
-            $("#ties").text(localTies);
-        } else {
-            playerTwoWins++;
-            database.ref("/gameInfo").update({
-                playerTwoWins: playerTwoWins,
-                playerOneChoice: blank,
-                playerTwoChoice: blank,
-            });
-            if(playerNumber == 1){
-                playerChoice = snapshot.val().playerOneChoice;
-                oppChoice = snapshot.val().playerTwoChoice;
-                wins = snapshot.val().playerOneWins;
-                losses = snapshot.val().playerTwoWins;
-                localTies = snapshot.val().ties;
-                $("#wins").text(wins);
-                $("#losses").text(losses);
-                $("#ties").text(localTies);
-            }else{
-                playerChoice = snapshot.val().playerTwoChoice;
-                oppChoice = snapshot.val().playerOneChoice;
-                wins = snapshot.val().playerTwoWins;
-                losses = snapshot.val().playerOneWins;
-                localTies = snapshot.val().ties;
-                $("#wins").text(wins);
-                $("#losses").text(losses);
-                $("#ties").text(localTies);
-            }
-        }
-    }else if(choices.includes(p1c)) {
-        if(playerNumber == 1){
-            $("#choice").text(snapshot.val().playerOneChoice);
-            $("#oppChoice").text("Waiting for Opponent to Choose");
-
-      // Change the HTML to reflect the local value in firebase
+        localTies = snapshot.val().ties;
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#ties").text(localTies);
+    } else {
+        playerTwoWins++;
+        $("#choice").text("Player One Chose " + snapshot.val().playerOneChoice + " and Player Two Chose " + snapshot.val().playerTwoChoice + ". Player Two Wins!")
+        $("#oppChoice").text("Make a another choice to play again!");
+        database.ref("/gameInfo").update({
+          playerTwoWins: playerTwoWins,
+          playerOneChoice: blank,
+          playerTwoChoice: blank,
+        });
+      }
+  }else if(choices.includes(p1c)) {
+    if(playerNumber == 1){
+      $("#choice").text("You chose " + snapshot.val().playerOneChoice);
+      $("#oppChoice").text("Waiting for Opponent to Choose");
         }else{
-            $("#choice").text("Choose your Destiny!");
-            $("#oppChoice").text("Your Opponent has Chosen!");
+          $("#choice").text("Choose your Destiny!");
+          $("#oppChoice").text("Your Opponent has Chosen!");
         }
-    }else if(choices.includes(p2c)) {
-        if(playerNumber == 2){
-        $("#choice").text(snapshot.val().playerTwoChoice);
-        $("#oppChoice").text("Waiting for Opponent to Choose");
-        }
-    }else if (connectedRef.numChildren > 1){
-        $("#choice").text("Choose your Destiny!");
-        $("#oppChoice").text("Waiting for Opponent to Choose");
+  }else if(choices.includes(p2c)) {
+    if(playerNumber == 2){
+      $("#choice").text("You chose " + snapshot.val().playerTwoChoice);
+      $("#oppChoice").text("Waiting for Opponent to Choose");
+    }
+  }else if (connectedRef.numChildren > 1){
+    $("#choice").text("Choose your Destiny!");
+    $("#oppChoice").text("Waiting for Opponent to Choose");
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#ties").text(localTies);
+  }
+  if(playerNumber == 1){
+    playerChoice = snapshot.val().playerOneChoice;
+    oppChoice = snapshot.val().playerTwoChoice;
+    wins = snapshot.val().playerOneWins;
+    losses = snapshot.val().playerTwoWins;
+    localTies = snapshot.val().ties;
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#ties").text(localTies);
+    }
+  if(playerNumber == 2){
+  playerChoice = snapshot.val().playerTwoChoice;
+  oppChoice = snapshot.val().playerOneChoice;
+  wins = snapshot.val().playerTwoWins;
+  losses = snapshot.val().playerOneWins;
+  localTies = snapshot.val().ties;
+  $("#wins").text(wins);
+  $("#losses").text(losses);
+  $("#ties").text(localTies);
     }
 
-    // If any errors are experienced, log them to console.
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
+}, function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
   
-document.onkeyup = function(event) {
-
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    
-    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-    if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-
-
-        // Save the new price in Firebase
-        if(playerNumber == 1){
-            database.ref("/gameInfo").update({
-                playerOneChoice: userGuess,
-                    });
-                    }
-        if(playerNumber == 2){
-            database.ref("/gameInfo").update({
-                playerTwoChoice: userGuess,
-            });
-        }
-    }
+function updateDB(){
+  if(playerNumber == 1){
+    database.ref("/gameInfo").update({
+      playerOneChoice: playerChoice
+    });
+  }
+  if(playerNumber == 2){
+    database.ref("/gameInfo").update({
+      playerTwoChoice: playerChoice,
+    });
+  }
 }
 
+$("#rock").on("click", function(){
+  $("#directions").text("");
+  if(playerNumber == 1){
+    if (DBOutput.child("playerOneChoice").val() == ""){
+    $("#choice").text("You chose Rock!");
+    playerChoice = "Rock"
+    updateDB()
+    }else{
+      $("#choice").text("No last minute switches brah! You chose " + DBOutput.child("playerOneChoice").val() + " already!")
+      }
+    }
+    else{
+    if (DBOutput.child("playerTwoChoice").val() == ""){
+      $("#choice").text("You chose Rock!");
+      playerChoice = "Rock"
+      updateDB()
+    }else{
+        $("#choice").text("No last minute switches brah! You chose " + DBOutput.child("playerTwoChoice").val() + " already!");
+      }
+    }
+})
 
-        // Hide the directions
+$("#paper").on("click", function(){
+  $("#directions").css("hide");
+  if(playerNumber == 1){
+    if (DBOutput.child("playerOneChoice").val() == ""){
+    $("#choice").text("You chose Paper!");
+    playerChoice = "Paper"
+    updateDB()
+    }else{
+      $("#choice").text("No last minute switches brah! You chose " + DBOutput.child("playerOneChoice").val() + " already!")
+      }
+    }
+    else{
+    if (DBOutput.child("playerTwoChoice").val() == ""){
+      $("#choice").text("You chose Paper!");
+      playerChoice = "Paper"
+      updateDB()
+    }else{
+        $("#choice").text("No last minute switches brah! You chose " + DBOutput.child("playerTwoChoice").val() + " already!");
+      }
+    }
+})
 
-        // directionsText.textContent = "";
+$("#scissors").on("click", function(){
+  $("#directions").text("");
+  if(playerNumber == 1){
+  if (DBOutput.child("playerOneChoice").val() == ""){
+  $("#choice").text("You chose Scissors!");
+  playerChoice = "Scissors"
+  updateDB()
+  }else{
+    $("#choice").text("No last minute switches brah! You chose " + DBOutput.child("playerOneChoice").val() + " already!")
+    }
+  }
+  else{
+  if (DBOutput.child("playerTwoChoice").val() == ""){
+    $("#choice").text("You chose Scissors!");
+    playerChoice = "Scissors"
+    updateDB()
+  }else{
+      $("#choice").text("No last minute switches brah! You chose " + DBOutput.child("playerTwoChoice").val() + " already!");
+    }
+  }
+})
